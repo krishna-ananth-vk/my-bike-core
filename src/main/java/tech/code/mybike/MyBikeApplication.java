927793809af32c28904d911e9b3fe425850029a7
ananthakrishnan <ananthakrishnan@nuchange.com>
2024-03-17 22:15:06 +0530

    spring setup

---
+++ src/main/java/tech/code/mybike/MyBikeApplication.java
@@ -0,0 +1,13 @@
+package tech.code.mybike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/code/mybike/MyBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.code.mybike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
